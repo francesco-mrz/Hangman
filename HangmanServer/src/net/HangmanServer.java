bcae4981bc63b10b393d5048a34690e9026f537c
Francesco <francesco.marzullo01@universitadipavia.it>
2021-05-04 17:24:54 +0200

    Update

--- HangmanServer/src/net/HangmanServer.java
+++ HangmanServer/src/net/HangmanServer.java
@@ -5,17 +5,58 @@
  */
 package net;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import hangman.Game;
+
 /**
  *
  * @author Claudio Cusano <claudio.cusano@unipv.it>
  */
 public class HangmanServer {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+	static int port = 8888;
+	static Socket socket;
+	static Game game;
+
+	/**
+	 * @param args the command line arguments
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		game = new Game("ciao");
+		try {
+			ServerSocket serverSocket = new ServerSocket(port, 0, InetAddress.getByName(null));
+			socket = serverSocket.accept();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+		while (true) {
+			String line = in.readLine();
+			if (line.length() == 1) {
+				char c = line.charAt(0);
+				System.out.println(line);
+				System.out.println(c);
+				game.makeAttempt(c);
+			}
+			if (line.length() > 1) {
+				System.out.println("Inserire un carattere");
+				line = in.readLine();
+			}
+			if (line.isEmpty()) {
+				System.out.println("Vuoto");
+				line = in.readLine();
+			}
+		}
+	}
 }
